b2493a552a2ce61e5036763a5440aa975d59a9be
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2024-02-04 01:19:41 +0900

    Reduce backdrop opacity

--- src/play.js
+++ src/play.js
@@ -1,17 +1,31 @@
 console.log('Better Afreeca')
 
 const scriptId = 'better_afreeca_embedded_script'
+const styleId = 'better_afreeca_embedded_style'
 
 const oldScript = document.getElementById(scriptId)
 if (oldScript) {
   console.debug('Removing old script')
   oldScript.remove()
 }
+const oldStyle = document.getElementById(styleId)
+if (oldStyle) {
+  console.debug('Removing old style')
+  oldStyle.remove()
+}
 
 const runtime = chrome?.runtime || browser?.runtime
+
 const script = document.createElement('script')
 script.src = runtime.getURL('src/play.embedded.js')
 script.id = scriptId
 
+const style = document.createElement('link')
+style.href = runtime.getURL('src/play.css')
+style.rel = "stylesheet"
+style.type = "text/css"
+style.id = styleId
+
 const head = document.head || document.documentElement
 head.appendChild(script)
+head.appendChild(style)
