b67c1145cca69559708ac96565d832a0600a6449
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2024-02-04 01:53:27 +0900

    Cleanup on reloading (#23)

--- src/play.embedded.js
+++ src/play.embedded.js
@@ -1,5 +1,8 @@
 ;(() => {
+  const scriptId = 'better_afreeca_embedded_script'
   const pasteAllowIds = ['write_area', 'auqa_voice_textarea']
+
+  let cleanups = []
 
   pasteAllowIds.forEach((id) => {
     const el = document.getElementById(id)
@@ -7,9 +10,29 @@
       console.debug(`${id} not found`)
       return
     }
-    el.addEventListener('focus', () => {
+
+    const unblock = () => {
       console.debug(`clearing ${id} paste blocker`)
       $(`#${id}`).off('cut copy paste')
+    }
+    el.addEventListener('focus', unblock)
+
+    cleanups.push(() => el.removeEventListener('focus', unblock))
+  })
+
+  const script = document.getElementById(scriptId)
+  const observer = new MutationObserver((mrs) => {
+    mrs.forEach((mr) => {
+      if (!mr?.removedNodes) {
+        return
+      }
+      mr.removedNodes.forEach((n) => {
+        if (n.id === script.id) {
+          observer.disconnect()
+          cleanups.forEach((f) => f())
+        }
+      })
     })
   })
+  observer.observe(script.parentElement, { childList: true })
 })()
